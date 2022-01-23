978e1f17e27d7c87ce27e03d8ec00b6270072543
Narutinn <69373580+Gabriel-bits@users.noreply.github.com>
2022-01-23 12:44:36 -0300

    up

--- aspirador.py
+++ aspirador.py
@@ -7,17 +7,30 @@ class Aspirador_de_po():
         self.diretorios_de_cache = []
         self.android = str
         self.armazenamento = int
+        self.cache_0 = None
+        self.cache_1 = None
+        self.cache_2 = None
 
     def analisador_de_cache(self):
-        # print(os.path.split('MeuAplicativo.kv'))
+        # print(os.path('MeuAplicativo.kv'))
         # print(os.path.getsize('MeuAplicativo.kv'))
         # print(os.getcwd())
         # print('1')
 
 
-        # for root, folder, files in os.walk('\\storage\\'):
-        #     print()
-        return os.getcwd()
+        for root, folder, files in os.walk('android/'):
+            if '\\cache' in root:
+                self.cache_0 = root
+                print('etp 2')
+                for root, folder, files in os.walk(self.cache_0):
+                    print(files)
+
+        # print(os.path.getsize("android/cache.txt"))
+        # size_file = os.path.getsize("android/cache.txt")
+
+        # return size_file
+
+        pass
 
     def naosei():
         pass
--- main.py
+++ main.py
@@ -1,38 +1,67 @@
 import kivy
+from kivy import utils
 from kivy.app import App
-from kivy.lang import Builder
+from kivy.core import window
+from kivy.graphics import Color, Rectangle
+from kivy.uix.behaviors.button import ButtonBehavior
 from kivy.uix.gridlayout import GridLayout
+from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
 from kivy.uix.screenmanager import Screen, ScreenManager
 from kivy.uix.button import Button
 from kivy.uix.gridlayout import GridLayout
-from kivy.uix.widget import Widget
+from kivy.core.window import Window
 from aspirador import Aspirador_de_po
 
-# INTERFACE = Builder.load_file("tela.kv")
+# INTERFACE = Builder.load_file("MeuAplicativo.kv")
 aspirador_de_cache = Aspirador_de_po
+# window.WindowBase.size = (1920,1080)
 
-class Botao(Button):
+Window.size = (4*73.5, 4*149)
+
+class Fatias(BoxLayout):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+class Botao(ButtonBehavior, Label):
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.color = (255, 1, 1 ,1)
 
-    def printar_batata(self):
-        print('batata')
+        print(utils.get_color_from_hex("#40004D"))
 
+        # self.color = (255, 1, 1 )
+        # c = (0.87,0.14,0.82)
+        # self.size = ("100dp", "10dp")
+        # self.size_hint_x = '100dp'
+        # self.size_hint_max_x = 150
+        # print(utils.get_color_from_hex("#0D002C"))
 
 class interface(GridLayout):
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+        # Label.color = (0.87,0.14,0.82)
         self.padding = 10
-        self.cols = 3
+        self.rows = 3
+        self.size_files = str(aspirador_de_cache.analisador_de_cache(self))
 
+    def on_size(self, *args):
+        self.canvas.before.clear()
+        with self.canvas.before:
+            Color(0.050980392156862744, 0.0, 0.17254901960784313, 1)
+            Rectangle(pos=self.pos, size=self.size)
 
     def click(self):
-        self.ids["apps_e_peso"].text = f'{aspirador_de_cache.analisador_de_cache(self)}'
-    
+        if type(self.ids["apps_e_peso"].text) == str:
+            number = self.ids["apps_e_peso"].text
+            number = int(number)
+            number += 1
+            self.ids["apps_e_peso"].text = str(number)
+
+
+        # f'{aspirador_de_cache.analisador_de_cache(self)}'
+
 
 class MeuAplicativo(App):
 
@@ -40,6 +69,7 @@ class MeuAplicativo(App):
     #     return INTERFACE
 
     pass
+
 
 aplicativo = MeuAplicativo()
 aplicativo.run()
